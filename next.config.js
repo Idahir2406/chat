cb23a0f2d7873ba570cf92a4d9a30f8d635788f1
Irving <idairreyes@gmail.com>
2024-01-14 13:31:02 -0500

    next fix

--- next.config.js
+++ next.config.js
@@ -5,8 +5,7 @@ const nextConfig = {
     NEXTAUTH_JWT_SECRET: "myjwtsecret",
     NEXTAUTH_JWT_SIGNING_KEY: "myjwtsigningkey",
     NEXTAUTH_JWT_ENCRYPTION_KEY: "myjwtencryptionkey",
-    NEXTAUTH_URL: "http://localhost:3000/",
-    NEXT_PUBLIC_API_URL: "http://localhost:3000",
+  
   },
   typescript: {
     // !! WARN !!
